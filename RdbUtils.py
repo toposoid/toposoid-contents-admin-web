5b8d31fb69fbcd35d62142ab99c539a8e130bef0
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2025-07-25 21:39:30 +0900

    https://github.com/toposoid/toposoid-contents-admin-web/issues/70

--- RdbUtils.py
+++ RdbUtils.py
@@ -28,6 +28,8 @@ UPLOAD_COMPLETED = 3
 VALIDATION_COMPLETED = 4
 ANALYSIS_COMPLETED = 5
 
+READ_TIMEOUT = 30.0
+
 def addDocumentAnalysisResultHistory(stateId, documentId, originalFilename, transversalStateJson, totalSeparatedNumber=-1):
     documentAnalysisResultHistoryRecord = DocumentAnalysisResultHistoryRecord(
         stateId = stateId, documentId=documentId, originalFilename=originalFilename, totalSeparatedNumber = totalSeparatedNumber
@@ -35,7 +37,7 @@ def addDocumentAnalysisResultHistory(stateId, documentId, originalFilename, tran
     requestJson = str(jsonable_encoder(documentAnalysisResultHistoryRecord)).replace("'", "\"")
     requestHeaders = {'Content-type': 'application/json', 'X_TOPOSOID_TRANSVERSAL_STATE': transversalStateJson.replace("'", "\"")}
     url =  f'http://{os.environ["TOPOSOID_RDB_WEB_HOST"]}:{os.environ["TOPOSOID_RDB_WEB_PORT"]}/addDocumentAnalysisResultHistory'
-    response = httpx.post(url , data=requestJson, headers=requestHeaders) 
+    response = httpx.post(url , data=requestJson, headers=requestHeaders, timeout=httpx.Timeout(10.0, read=READ_TIMEOUT)) 
     print(response)
 
 def addKnowledgeRegisterHistory(stateId, documentId, sequentialNumber, propositionId, sentences, json, transversalStateJson):
@@ -44,7 +46,7 @@ def addKnowledgeRegisterHistory(stateId, documentId, sequentialNumber, propositi
     requestJson = str(jsonable_encoder(knowledgeRegisterHistoryRecord)).replace("'", "\"")
     requestHeaders = {'Content-type': 'application/json', 'X_TOPOSOID_TRANSVERSAL_STATE': transversalStateJson.replace("'", "\"")}
     url =  f'http://{os.environ["TOPOSOID_RDB_WEB_HOST"]}:{os.environ["TOPOSOID_RDB_WEB_PORT"]}/addKnowledgeRegisterHistory'
-    response = httpx.post(url , data=requestJson, headers=requestHeaders) 
+    response = httpx.post(url , data=requestJson, headers=requestHeaders, timeout=httpx.Timeout(10.0, read=READ_TIMEOUT)) 
     print(response)
 
 def searchDocumentAnalysisResultHistoryByDocumentIdAndStateId(documentId, stateId, transversalStateJson):
@@ -54,26 +56,26 @@ def searchDocumentAnalysisResultHistoryByDocumentIdAndStateId(documentId, stateI
     requestJson = str(jsonable_encoder(documentAnalysisResultHistoryRecord)).replace("'", "\"")
     requestHeaders = {'Content-type': 'application/json', 'X_TOPOSOID_TRANSVERSAL_STATE': transversalStateJson.replace("'", "\"")}
     url =  f'http://{os.environ["TOPOSOID_RDB_WEB_HOST"]}:{os.environ["TOPOSOID_RDB_WEB_PORT"]}/searchDocumentAnalysisResultHistoryByDocumentIdAndStateId'
-    response = httpx.post(url , data=requestJson, headers=requestHeaders)     
+    response = httpx.post(url , data=requestJson, headers=requestHeaders, timeout=httpx.Timeout(10.0, read=READ_TIMEOUT))     
     return parse_obj_as(List[DocumentAnalysisResultHistoryRecord], response.json())
 
 def getKnowledgeRegisterHistoryTotalCountByDocumentId(knowledgeRegisterHistoryCount, transversalStateJson):
     requestJson = str(jsonable_encoder(knowledgeRegisterHistoryCount)).replace("'", "\"")
     requestHeaders = {'Content-type': 'application/json', 'X_TOPOSOID_TRANSVERSAL_STATE': transversalStateJson.replace("'", "\"")}
     url =  f'http://{os.environ["TOPOSOID_RDB_WEB_HOST"]}:{os.environ["TOPOSOID_RDB_WEB_PORT"]}/getKnowledgeRegisterHistoryTotalCountByDocumentId'
-    response = httpx.post(url , data=requestJson, headers=requestHeaders)     
+    response = httpx.post(url , data=requestJson, headers=requestHeaders, timeout=httpx.Timeout(10.0, read=READ_TIMEOUT))     
     return parse_obj_as(KnowledgeRegisterHistoryCount, response.json())
 
 def getKnowledgeRegisterHistoryCountByDocumentId(knowledgeRegisterHistoryCount, transversalStateJson):
     requestJson = str(jsonable_encoder(knowledgeRegisterHistoryCount)).replace("'", "\"")
     requestHeaders = {'Content-type': 'application/json', 'X_TOPOSOID_TRANSVERSAL_STATE': transversalStateJson.replace("'", "\"")}
     url =  f'http://{os.environ["TOPOSOID_RDB_WEB_HOST"]}:{os.environ["TOPOSOID_RDB_WEB_PORT"]}/getKnowledgeRegisterHistoryCountByDocumentId'
-    response = httpx.post(url , data=requestJson, headers=requestHeaders)     
+    response = httpx.post(url , data=requestJson, headers=requestHeaders, timeout=httpx.Timeout(10.0, read=READ_TIMEOUT))     
     return parse_obj_as(KnowledgeRegisterHistoryCount, response.json())
 
 def searchLatestDocumentAnalysisStateByDocumentId(documentAnalysisResultHistoryRecord, transversalStateJson):
     requestJson = str(jsonable_encoder(documentAnalysisResultHistoryRecord)).replace("'", "\"")
     requestHeaders = {'Content-type': 'application/json', 'X_TOPOSOID_TRANSVERSAL_STATE': transversalStateJson.replace("'", "\"")}
     url =  f'http://{os.environ["TOPOSOID_RDB_WEB_HOST"]}:{os.environ["TOPOSOID_RDB_WEB_PORT"]}/searchLatestDocumentAnalysisStateByDocumentId'
-    response = httpx.post(url , data=requestJson, headers=requestHeaders)     
+    response = httpx.post(url , data=requestJson, headers=requestHeaders, timeout=httpx.Timeout(10.0, read=READ_TIMEOUT))     
     return parse_obj_as(List[DocumentAnalysisResultHistoryRecord], response.json())
