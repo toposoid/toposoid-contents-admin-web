bcdf2e02c0b08eb8efe2e494853dda5b1294632d
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2026-08-03 10:33:08 +0000

    https://github.com/toposoid/toposoid-contents-admin-web/issues/86

--- test_api.py
+++ test_api.py
@@ -55,11 +55,11 @@ class TestToposoidContentsAdminWeb(object):
             os.remove('contents/images/' + cls.id2 + ".jpeg")
 
     def test_transferFile(self):
-        target = f"tmp/{str(uuid.uuid4())}.png"
-        shutil.copy("IMG_TEST.png",target)
+        target = f"tmp/{str(uuid.uuid4())}.jpg"
+        shutil.copy("IMAGE_TEST.jpg",target)
 
         with open(target, "rb") as f:
-            response = self.client.post("/transferFile", headers={"X_TOPOSOID_TRANSVERSAL_STATE": self.transversalState},files={"uploadfile": (target.split('/')[-1], f, "image/png")})
+            response = self.client.post("/transferFile", headers={"X_TOPOSOID_TRANSVERSAL_STATE": self.transversalState},files={"uploadfile": (target.split('/')[-1], f, "image/jpeg")})
         assert response.status_code == status.HTTP_200_OK
         statusInfo = parse_obj_as(StatusInfo, response.json())
         assert(statusInfo.status == "OK")        
@@ -93,7 +93,7 @@ class TestToposoidContentsAdminWeb(object):
         assert registImageContentResult.statusInfo.status == "OK"        
         assert os.path.exists(f"contents/images/{registImageContentResult.knowledgeForImage.id}.jpg")
 
-    """
+    
     def test_registerTable(self):
         featureId = str(uuid.uuid4())
         target = f"contents/temporaryUse/{featureId}.xlsx"
@@ -102,26 +102,27 @@ class TestToposoidContentsAdminWeb(object):
         response = self.client.post("/registerTable",
                             headers={"Content-Type": "application/json", "X_TOPOSOID_TRANSVERSAL_STATE": self.transversalState},
                             json={
-                                "id": self.id1,
+                                "id": featureId,
                                 "tableReference":{
                                 "reference": {
                                     "url": f"{os.environ['TOPOSOID_CONTENTS_URL']}{target}",
-                                    "surface": "猫が",
+                                    "surface": "データが",
                                     "surfaceIndex": "0",
                                     "isWholeSentence": False,
-                                    "originalUrlOrReference": "http://images.cocodataset.org/val2017/000000039769.jpg",
+                                    "originalUrlOrReference": "",
                                     "metaInformations": []
                                 },
-                                "x": 27,
-                                "y": 41,
-                                "width": 287,
-                                "height": 435}
+                                "skipHeaderRows":0,
+                                "skipRowList":[],
+                                "multiHeaderRowsForExcel":1, 
+                                "sheetNameForExcel": ""
+                                }
                             })
         assert response.status_code == 200
         registTableContentResult = RegistTableContentResult.parse_obj(response.json())
         assert registTableContentResult.statusInfo.status == "OK"        
-        assert os.path.exists(f"contents/tables/{registTableContentResult.knowledgeForTable.id}.jpg")
-    """
+        assert os.path.exists(f"contents/tables/{registTableContentResult.knowledgeForTable.id}.xlsx")
+    
     def test_registerDocument(self):
         documentId = str(uuid.uuid4())
         target = f"contents/temporaryUse/{documentId}.pdf"
@@ -225,137 +226,4 @@ class TestToposoidContentsAdminWeb(object):
         assert documentAnalysisResultHistories[0].documentId == documentId
         assert documentAnalysisResultHistories[0].stateId == 3
 
-    """
-    def test_registImage(self): 
-        
-        response = self.client.post("/registImage",
-                            headers={"Content-Type": "application/json", "X_TOPOSOID_TRANSVERSAL_STATE": self.transversalState},
-                            json={
-                                "id": self.id1,
-                                "imageReference":{
-                                "reference": {
-                                    "url": "",
-                                    "surface": "猫が",
-                                    "surfaceIndex": "0",
-                                    "isWholeSentence": False,
-                                    "originalUrlOrReference": "http://images.cocodataset.org/val2017/000000039769.jpg",
-                                    "metaInformations": []
-                                },
-                                "x": 27,
-                                "y": 41,
-                                "width": 287,
-                                "height": 435}
-                            })
-        assert response.status_code == 200
-        registContentResult = RegistImageContentResult.parse_obj(response.json())
-        assert registContentResult.statusInfo.status == "OK"
-        assert registContentResult.knowledgeForImage.imageReference.reference.url == os.environ["TOPOSOID_CONTENTS_URL"] + "images/" + self.id1 + ".jpg"
-        assert os.path.exists('contents/images/' + self.id1 + "-org.jpeg")
-        assert os.path.exists('contents/images/' + self.id1 + ".jpg")
 
-
-    def test_registImage2(self): 
-        
-        response = self.client.post("/registImage",
-                            headers={"Content-Type": "application/json", "X_TOPOSOID_TRANSVERSAL_STATE": self.transversalState},
-                            json={
-                                "id": self.id2,
-                                "imageReference":{
-                                "reference": {
-                                    "url": "",
-                                    "surface": "",
-                                    "surfaceIndex": "-1",
-                                    "isWholeSentence": True,
-                                    "originalUrlOrReference": "http://images.cocodataset.org/train2017/000000428746.jpg",
-                                    "metaInformations": []
-                                },
-                                "x": 0,
-                                "y": 0,
-                                "width": 0,
-                                "height": 0}
-                            })
-        assert response.status_code == 200
-        registContentResult = RegistImageContentResult.parse_obj(response.json())
-        assert registContentResult.statusInfo.status == "OK"
-        assert registContentResult.knowledgeForImage.imageReference.reference.url == os.environ["TOPOSOID_CONTENTS_URL"] + "images/" + self.id2 + ".jpg"
-        assert os.path.exists('contents/images/' + self.id2 + "-org.jpeg")
-        assert os.path.exists('contents/images/' + self.id2 + ".jpg")
-
-    def test_uploadTemporaryImage(self): 
-        
-        response = self.client.post("/uploadTemporaryImage",
-                            headers={"Content-Type": "application/json", "X_TOPOSOID_TRANSVERSAL_STATE": self.transversalState},
-                            json={
-                                "id": self.id1,
-                                "imageReference":{
-                                "reference": {
-                                    "url": "",
-                                    "surface": "猫が",
-                                    "surfaceIndex": "0",
-                                    "isWholeSentence": False,
-                                    "originalUrlOrReference": "http://images.cocodataset.org/val2017/000000039769.jpg",
-                                    "metaInformations": []
-                                },
-                                "x": 27,
-                                "y": 41,
-                                "width": 287,
-                                "height": 435}
-                            })
-        assert response.status_code == 200
-        registContentResult = RegistImageContentResult.parse_obj(response.json())
-        assert registContentResult.statusInfo.status == "OK"
-        assert registContentResult.knowledgeForImage.imageReference.reference.url == os.environ["TOPOSOID_CONTENTS_URL"] + "temporaryUse/" + self.id1 + ".jpg"
-        assert os.path.exists('contents/temporaryUse/' + self.id1 + "-org.jpeg")
-        assert os.path.exists('contents/temporaryUse/' + self.id1 + ".jpg")
-
-    def test_uploadTemporaryImage2(self): 
-        
-        response = self.client.post("/uploadTemporaryImage",
-                            headers={"Content-Type": "application/json", "X_TOPOSOID_TRANSVERSAL_STATE": self.transversalState},
-                            json={
-                                "id": self.id2,
-                                "imageReference":{
-                                "reference": {
-                                    "url": "",
-                                    "surface": "",
-                                    "surfaceIndex": "-1",
-                                    "isWholeSentence": True,
-                                    "originalUrlOrReference": "http://images.cocodataset.org/train2017/000000428746.jpg",
-                                    "metaInformations": []
-                                },
-                                "x": 0,
-                                "y": 0,
-                                "width": 0,
-                                "height": 0}
-                            })
-        assert response.status_code == 200
-        registContentResult = RegistImageContentResult.parse_obj(response.json())
-        assert registContentResult.statusInfo.status == "OK"
-        assert registContentResult.knowledgeForImage.imageReference.reference.url == os.environ["TOPOSOID_CONTENTS_URL"] + "temporaryUse/" + self.id2 + ".jpg"
-        assert os.path.exists('contents/temporaryUse/' + self.id2 + "-org.jpeg")
-        assert os.path.exists('contents/temporaryUse/' + self.id2 + ".jpg")
-
-    def test_uploadImageFile(self):
-        with open("IMG_TEST.png", "rb") as f:
-            response = self.client.post("/uploadImageFile", headers={"X_TOPOSOID_TRANSVERSAL_STATE": self.transversalState},files={"uploadfile": ("IMG_TEST.png", f, "image/png")})
-        assert response.status_code == status.HTTP_200_OK
-        print(response.json())
-    
-    def test_uploadDocumentFile(self):
-        with open("JAPANESE_DOCUMENT_FOR_TEST.pdf", "rb") as f:
-            response = self.client.post("/uploadDocumentFile", headers={"X_TOPOSOID_TRANSVERSAL_STATE": self.transversalState},files={"uploadfile": ("JAPANESE_DOCUMENT_FOR_TEST.pdf", f, "application/pdf")})
-        assert response.status_code == status.HTTP_200_OK
-        #pprint.pprint(response)
-        document = Document.parse_obj(response.json())
-        assert document.filename == "JAPANESE_DOCUMENT_FOR_TEST.pdf"
-        assert os.path.exists('contents/documents/' + document.documentId + ".pdf" )
-        assert os.path.exists('contents/documents/' + document.documentId + "-" + document.filename )
-        documentAnalysisResultHistories = searchDocumentAnalysisResultHistoryByDocumentIdAndStateId(document.documentId, UPLOAD_COMPLETED, self.transversalState)
-        assert len(documentAnalysisResultHistories) == 1
-        assert documentAnalysisResultHistories[0].documentId == document.documentId
-        assert documentAnalysisResultHistories[0].stateId == UPLOAD_COMPLETED
-        documentRegistrationJson = receiveMessage(TOPOSOID_MQ_DOCUMENT_ANALYSIS_QUENE)
-        documentRegistration = DocumentRegistration.parse_raw(documentRegistrationJson)
-        assert documentRegistration.document.documentId == document.documentId
-
-    """
